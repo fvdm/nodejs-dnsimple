d8e3dda5cd1386b28afea44498fea6cbfc35f083
Franklin van de Meent <fr@nkl.in>
2014-05-25 23:27:47 +0200

    Try to get login details from argv, env or nothing

--- test.js
+++ test.js
@@ -1,9 +1,14 @@
-// Setup: test -email=me@some.where -token=abc123
 var opts = {}
 for( var i=2; i < process.argv.length; i++ ) {
 	var p = process.argv[i].match(/-(\w+)=([^ ]+)/)
 	opts[ p[1] ] = p[2]
 }
 
-var ds = require('./')({email: opts.email, token: opts.token})
+// Setup
+// set env API_EMAIL and API_TOKEN (Travis CI)
+// or use cli arguments -email=me@some.where -token=abc123
+var ds = require('./')({
+	email: opts.email || process.env.API_EMAIL || null,
+	token: opts.token || process.env.API_TOKEN || null
+})
 
