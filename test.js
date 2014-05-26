6d80538a36d8c9dd137510fa1301b98aad760932
Franklin van de Meent <fr@nkl.in>
2014-05-26 21:24:19 +0200

    Test: added basics

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+// cli arguments
 var opts = {}
 for( var i=2; i < process.argv.length; i++ ) {
 	var p = process.argv[i].match(/-(\w+)=([^ ]+)/)
@@ -12,3 +13,39 @@ var ds = require('./')({
 	token: opts.token || process.env.API_TOKEN || null
 })
 
+// handle exits
+var errors = 0
+process.on( 'exit', function() {
+	if( errors == 0 ) {
+		console.log('\nDONE, no errors.\n')
+		process.exit(0)
+	} else {
+		console.log('\nFAIL, '+ errors +' error'+ (errors > 1 ? 's' : '') +' occurred!\n')
+		process.exit(1)
+	}
+})
+
+// doTest( passErr, 'methods', [
+//   ['feeds', typeof feeds, 'object']
+// ])
+function doTest( err, label, tests ) {
+	if( err instanceof Error ) {
+		console.log( label +': ERROR' )
+		console.log( err, err.stack )
+		errors++
+	} else {
+		var testErrors = []
+		tests.forEach( function( test ) {
+			if( test[1] !== test[2] ) {
+				testErrors.push(test[0])
+				errors++
+			}
+		})
+		
+		if( testErrors.length == 0 ) {
+			console.log( label +': ok' )
+		} else {
+			console.log( label +': failed ('+ testErrors.join(', ') +')' )
+		}
+	}
+}
