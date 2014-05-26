8f945e184c77e8d8e9b7b52ab389ef1938c053d5
Franklin van de Meent <fr@nkl.in>
2014-05-26 23:11:05 +0200

    Test: CLI arguments are passed by npm via process.env

--- test.js
+++ test.js
@@ -1,16 +1,9 @@
-// cli arguments
-var opts = {}
-for( var i=2; i < process.argv.length; i++ ) {
-	var p = process.argv[i].match(/-(\w+)=([^ ]+)/)
-	opts[ p[1] ] = p[2]
-}
-
 // Setup
 // set env API_EMAIL and API_TOKEN (Travis CI)
-// or use cli arguments -email=me@some.where -token=abc123
+// or use cli arguments: npm test --email=me@some.where --token=abc123
 var ds = require('./')({
-	email: opts.email || process.env.API_EMAIL || null,
-	token: opts.token || process.env.API_TOKEN || null
+	email: process.env.API_EMAIL || process.env.npm_config_email || null,
+	token: process.env.API_TOKEN || process.env.npm_config_token || null
 })
 
 // handle exits
