907a742eacbb65b14810aefc41d05fd021422b08
Franklin van de Meent <fr@nkl.in>
2014-05-27 17:56:27 +0200

    Test: further enhanced status output

--- test.js
+++ test.js
@@ -12,10 +12,10 @@ var ds = require('./')({
 var errors = 0
 process.on( 'exit', function() {
 	if( errors == 0 ) {
-		console.log('\nDONE, no errors.\n')
+		console.log('\n\033[1mDONE, no errors.\033[0m\n')
 		process.exit(0)
 	} else {
-		console.log('\nFAIL, '+ errors +' error'+ (errors > 1 ? 's' : '') +' occurred!\n')
+		console.log('\n\033[1mFAIL, '+ errors +' error'+ (errors > 1 ? 's' : '') +' occurred!\033[0m\n')
 		process.exit(1)
 	}
 })
@@ -39,7 +39,7 @@ function doNext() {
 // ])
 function doTest( err, label, tests ) {
 	if( err instanceof Error ) {
-		console.error( label +': \033[31mERROR\033[0m\n' )
+		console.error( label +': \033[1m\033[31mERROR\033[0m\n' )
 		console.error( util.inspect(err, false, 10, true) )
 		console.log()
 		console.error( err.stack )
@@ -55,9 +55,9 @@ function doTest( err, label, tests ) {
 		})
 		
 		if( testErrors.length == 0 ) {
-			console.log( label +': ok' )
+			console.log( label +': \033[1mok\033[0m' )
 		} else {
-			console.error( label +': \033[31mfailed\033[0m ('+ testErrors.join(', ') +')' )
+			console.error( label +': \033[1m\033[31mfailed\033[0m ('+ testErrors.join(', ') +')' )
 		}
 	}
 	
@@ -141,7 +141,7 @@ queue.push( function() {
 			errors++
 			process.exit(1)
 		} else {
-			console.log('API access: ok')
+			console.log('API access: \033[1mok\033[0m')
 			doNext()
 		}
 	})
