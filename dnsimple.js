f94be0e07b86a1bdcf6761f328a8b9e709ed5624
Franklin van de Meent <fr@nkl.in>
2014-10-29 10:56:34 +0100

    Added method domains.nameserver_register

--- dnsimple.js
+++ dnsimple.js
@@ -296,6 +296,15 @@ app.domains = {
       app.talk( 'DELETE', 'domains/'+ domainname +'/whois_privacy', callback )
     }
   },
+  
+  // domains.nameserver_register
+  nameserver_register: function( domainname, name, ip, callback ) {
+    var vars = {
+      name: name,
+      ip: ip
+    }
+    app.talk( 'POST', 'domains/'+ domainname +'/registry_name_servers', vars, callback )
+  },
 
 
   // SERVICES
