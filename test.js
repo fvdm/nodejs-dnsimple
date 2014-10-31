be1c02c7507a3f03c88f6135a2b43d4f48f369cc
Franklin van de Meent <fr@nkl.in>
2014-10-31 11:57:57 +0100

    Tests: added templates.apply

--- test.js
+++ test.js
@@ -474,6 +474,16 @@ queue.push( function() {
   })
 })
 
+// ! templates.apply
+queue.push( function() {
+  ds.templates.apply( bogus.domain.name, bogus.template.id, function( err, data, meta ) {
+    doTest( err, 'templates.apply', [
+      ['result', meta.statusCode === 200],
+      ['data', data.id === bogus.domain.id]
+    ])
+  })
+})
+
 // ! templates.records.delete
 queue.push( function() {
   ds.templates.records.delete( bogus.template.id, bogus.template_record.id, function( err, data, meta ) {
