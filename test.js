f7143b9fa080c50939fd58d1d950d254b5104528
Franklin van de Meent <fr@nkl.in>
2014-11-06 14:05:31 +0100

    Tests: domains.memberships.list (#10)

--- test.js
+++ test.js
@@ -298,9 +298,7 @@ queue.push( function() {
 // ! domains.memberships.list
 queue.push( function() {
   ds.domains.memberships.list( bogus.domain.name, function( err, data, meta ) {
-    doTest( err, 'domains.memberships.list', [
-      ['data type', meta.statusCode === 200 && data instanceof Array]
-    ])
+    doTest( err, 'domains.memberships.list', testArrObj(data))
   })
 })
 
