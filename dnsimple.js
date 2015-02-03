decae02eda36dfdeffbff444c89ae26a4ec957c6
Franklin van de Meent <fr@nkl.in>
2015-02-03 13:58:15 +0100

    Clean up code style

--- dnsimple.js
+++ dnsimple.js
@@ -12,7 +12,7 @@ Service API:   http://developer.dnsimple.com
 */
 
 
-module.exports = function( setup ) {
+module.exports = function (setup) {
   // ! Defaults
   var api = {
     hostname: setup.hostname || 'api.dnsimple.com',
@@ -26,56 +26,56 @@ module.exports = function( setup ) {
   };
 
   // ! API
-  return function( method, path, fields, callback ) {
-    if( !callback && typeof fields === 'function' ) {
+  return function (method, path, fields, callback) {
+    if (!callback && typeof fields === 'function') {
       callback = fields;
       fields = {};
     }
 
     // process callback data
     var complete = false;
-    function doCallback( err, data, meta ) {
-      if( !complete ) {
+    function doCallback (err, data, meta) {
+      if (!complete) {
         complete = true;
 
-        if( err ) {
-          callback( err, null, meta );
+        if (err) {
+          callback (err, null, meta);
           return;
         }
 
-        if( method === 'DELETE' && !(data instanceof Object && Object.keys(data).length > 0) ) {
-          callback( null, meta.statusCode === 200 || meta.statusCode === 204, meta );
+        if (method === 'DELETE' && !(data instanceof Object && Object.keys (data).length > 0)) {
+          callback (null, meta.statusCode === 200 || meta.statusCode === 204, meta);
           return;
         }
 
-        callback( null, data, meta );
+        callback (null, data, meta);
       }
     }
 
     // credentials set?
-    if( ! (api.email && api.token) && ! (api.email && api.password) && ! api.domainToken && ! api.twoFactorToken ) {
-      doCallback( new Error('credentials missing') );
+    if (! (api.email && api.token) && ! (api.email && api.password) && ! api.domainToken && ! api.twoFactorToken) {
+      doCallback (new Error('credentials missing'));
       return;
     }
 
     // prepare
-    var querystr = JSON.stringify(fields);
+    var querystr = JSON.stringify (fields);
     var headers = {
       'Accept': 'application/json',
       'User-Agent': 'Nodejs-DNSimple'
     };
 
     // token in headers
-    if( api.token ) {
+    if (api.token) {
       headers['X-DNSimple-Token'] = api.email +':'+ api.token;
     }
 
-    if( api.domainToken ) {
+    if (api.domainToken) {
       headers['X-DNSimple-Domain-Token'] = api.domainToken;
     }
 
     // build request
-    if( method.match( /(POST|PUT|DELETE)/ ) ) {
+    if (method.match ( /(POST|PUT|DELETE)/ )) {
       headers['Content-Type'] = 'application/json';
       headers['Content-Length'] = querystr.length;
     }
@@ -89,57 +89,57 @@ module.exports = function( setup ) {
     };
 
     // password authentication
-    if( ! api.twoFactorToken && ! api.token && ! api.domainToken && api.password && api.email ) {
+    if (! api.twoFactorToken && ! api.token && ! api.domainToken && api.password && api.email) {
       options.auth = api.email +':'+ api.password;
 
       // two-factor authentication (2FA)
-      if( api.twoFactorOTP ) {
+      if (api.twoFactorOTP) {
         headers['X-DNSimple-2FA-Strict'] = 1;
         headers['X-DNSimple-OTP'] = api.twoFactorOTP;
       }
     }
 
-    if( api.twoFactorToken ) {
+    if (api.twoFactorToken) {
       options.auth = api.twoFactorToken +':x-2fa-basic';
       headers['X-DNSimple-2FA-Strict'] = 1;
     }
 
     // start request
-    var request = require('https').request( options );
+    var request = require ('https').request (options);
 
     // response
-    request.on( 'response', function( response ) {
+    request.on ('response', function (response) {
       var meta = {statusCode: null};
       var data = [];
       var size = 0;
 
-      response.on( 'data', function( chunk ) {
-        data.push( chunk );
+      response.on ('data', function (chunk) {
+        data.push (chunk);
         size += chunk.length;
       });
 
-      response.on( 'close', function() {
-        doCallback( new Error('connection dropped') );
+      response.on ('close', function() {
+        doCallback (new Error('connection dropped'));
       });
 
       // request finished
-      response.on( 'end', function() {
-        data = new Buffer.concat( data, size ).toString('utf8').trim();
+      response.on ('end', function() {
+        data = new Buffer.concat (data, size).toString ('utf8').trim ();
         var failed = null;
 
         meta.statusCode = response.statusCode;
         meta.request_id = response.headers['x-request-id'];
         meta.runtime = response.headers['x-runtime'];
 
-        if( typeof response.headers['x-dnsimple-otp-token'] === 'string' ) {
+        if (typeof response.headers['x-dnsimple-otp-token'] === 'string') {
           meta.twoFactorToken = response.headers['x-dnsimple-otp-token'];
         }
 
-        if( response.statusCode !== 204 ) {
+        if (response.statusCode !== 204) {
           try {
-            data = JSON.parse( data );
-          } catch(e) {
-            doCallback(new Error('not json'), data);
+            data = JSON.parse (data);
+          } catch (e) {
+            doCallback (new Error ('not json'), data);
           }
         }
 
@@ -148,59 +148,60 @@ module.exports = function( setup ) {
         var error = null;
 
         // status ok, no data
-        if( data === '' && meta.statusCode < 300 ) {
+        if (data === '' && meta.statusCode < 300) {
           noError = true;
         }
+
         // domain check 404 = free
-        if( path.match(/^domains\/.+\/check$/) && meta.statusCode === 404 ) {
+        if (path.match ( /^domains\/.+\/check$/ ) && meta.statusCode === 404) {
           noError = true;
         }
 
         // check HTTP status code
-        if( noError || (!failed && response.statusCode < 300) ) {
-          doCallback( null, data, meta );
+        if (noError || (!failed && response.statusCode < 300)) {
+          doCallback ( null, data, meta );
         } else {
-          if( response.statusCode === 401 && response.headers['x-dnsimple-otp'] === 'required' ) {
-            error = new Error('twoFactorOTP required');
+          if (response.statusCode === 401 && response.headers['x-dnsimple-otp'] === 'required') {
+            error = new Error ('twoFactorOTP required');
           } else {
             error = failed || new Error('API error');
           }
           error.code = response.statusCode;
-          error.error = data.message || data.error || (data.errors && data instanceof Object && Object.keys(data.errors)[0] ? data.errors[ Object.keys(data.errors)[0] ] : null) || null;
+          error.error = data.message || data.error || (data.errors && data instanceof Object && Object.keys (data.errors)[0] ? data.errors[ Object.keys (data.errors)[0] ] : null) || null;
           error.data = data;
-          doCallback( error, null, meta );
+          doCallback (error, null, meta);
         }
       });
     });
 
     // timeout
-    request.on( 'socket', function( socket ) {
-      if( typeof api.timeout === 'number' ) {
-        socket.setTimeout( api.timeout );
-        socket.on( 'timeout', function() {
-          doCallback( new Error('request timeout') );
-          request.abort();
+    request.on ('socket', function (socket) {
+      if (typeof api.timeout === 'number') {
+        socket.setTimeout (api.timeout);
+        socket.on ('timeout', function() {
+          doCallback (new Error ('request timeout'));
+          request.abort ();
         });
       }
     });
 
     // error
-    request.on( 'error', function( error ) {
+    request.on ('error', function (error) {
       var er = null;
-      if( error.code === 'ECONNRESET' ) {
-        er = new Error('request timeout');
+      if (error.code === 'ECONNRESET') {
+        er = new Error ('request timeout');
       } else {
-        er = new Error('request failed');
+        er = new Error ('request failed');
       }
       er.error = error;
-      doCallback( er );
+      doCallback (er);
     });
 
     // run it
-    if( method.match( /(POST|PUT|DELETE)/ ) ) {
-      request.end( querystr );
+    if (method.match ( /(POST|PUT|DELETE)/ )) {
+      request.end (querystr);
     } else {
-      request.end();
+      request.end ();
     }
   };
 };
