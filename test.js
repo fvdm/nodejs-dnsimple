c5eb525596e8979235bd093c52ba6974438da102
Franklin van de Meent <fr@nkl.in>
2015-02-03 14:13:08 +0100

    Test: clean up code style

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
-var testStart = Date.now();
-var util = require('util');
+var testStart = Date.now ();
+var util = require ('util');
 
 // Setup
 // set env DNSIMPLE_EMAIL and DNSIMPLE_TOKEN (Travis CI)
@@ -14,36 +14,36 @@ var token = process.env.npm_config_dnstoken || process.env.DNSIMPLE_TOKEN || nul
 var pass = process.env.npm_config_dnspass || process.env.DNSIMPLE_PASS || null;
 var otp = process.env.npm_config_dnsotp || process.env.DNSIMPLE_OTP || null;
 
-if( pass && otp ) {
+if (pass && otp) {
   acc.password = pass;
   acc.twoFactorOTP = otp;
-} else if( token ) {
+} else if (token) {
   acc.token = token;
 }
 
-var app = require('./')( acc );
+var app = require ('./') (acc);
 
 // handle exits
 var errors = 0;
-process.on( 'exit', function() {
-  var testTime = Date.now() - testStart;
-  if( errors === 0 ) {
-    console.log('\n\033[1mDONE, no errors.\033[0m');
-    console.log('Timing: \033[33m%s ms\033[0m\n', testTime);
-    process.exit(0);
+process.on ('exit', function () {
+  var testTime = Date.now () - testStart;
+  if (errors === 0) {
+    console.log ('\n\033[1mDONE, no errors.\033[0m');
+    console.log ('Timing: \033[33m%s ms\033[0m\n', testTime);
+    process.exit (0);
   } else {
-    console.log('\n\033[1mFAIL, '+ errors +' error'+ (errors > 1 ? 's' : '') +' occurred!\033[0m');
-    console.log('Timing: \033[33m%s ms\033[0m\n', testTime);
-    process.exit(1);
+    console.log ('\n\033[1mFAIL, '+ errors +' error'+ (errors > 1 ? 's' : '') +' occurred!\033[0m');
+    console.log ('Timing: \033[33m%s ms\033[0m\n', testTime);
+    process.exit (1);
   }
 });
 
 // prevent errors from killing the process
-process.on( 'uncaughtException', function( err ) {
-  console.log();
-  console.error( err.stack );
-  console.trace();
-  console.log();
+process.on ('uncaughtException', function (err) {
+  console.log ();
+  console.error (err.stack);
+  console.trace ();
+  console.log ();
   errors++;
 });
 
@@ -51,54 +51,54 @@ process.on( 'uncaughtException', function( err ) {
 var queue = [];
 var next = 0;
 
-function doNext() {
+function doNext () {
   next++;
-  if( queue[next] ) {
-    queue[next]();
+  if (queue[next]) {
+    queue[next] ();
   }
 }
 
-// doTest( passErr, 'methods', [
+// doTest (passErr, 'methods', [
 //   ['feeds', typeof feeds === 'object']
 // ]);
-function doTest( err, label, tests ) {
-  if( err instanceof Error ) {
-    console.error( label +': \033[1m\033[31mERROR\033[0m\n' );
-    console.error( util.inspect(err, false, 10, true) );
-    console.log();
-    console.error( err.stack );
-    console.log();
+function doTest (err, label, tests) {
+  if (err instanceof Error) {
+    console.error (label +': \033[1m\033[31mERROR\033[0m\n');
+    console.error (util.inspect (err, false, 10, true));
+    console.log ();
+    console.error (err.stack);
+    console.log ();
     errors++;
   } else {
     var testErrors = [];
-    tests.forEach( function( test ) {
-      if( test[1] !== true ) {
-        testErrors.push(test[0]);
+    tests.forEach (function (test) {
+      if (test[1] !== true) {
+        testErrors.push (test[0]);
         errors++;
       }
     });
 
-    if( testErrors.length === 0 ) {
-      console.log( label +': \033[1m\033[32mok\033[0m' );
+    if (testErrors.length === 0) {
+      console.log (label +': \033[1m\033[32mok\033[0m');
     } else {
-      console.error( label +': \033[1m\033[31mfailed\033[0m ('+ testErrors.join(', ') +')' );
+      console.error (label +': \033[1m\033[31mfailed\033[0m ('+ testErrors.join (', ') +')');
     }
   }
 
-  doNext();
+  doNext ();
 }
 
 // First check API access
-queue.push( function() {
-  app( 'GET', '/prices', function(err) {
-    if(err) {
-      console.log('API access: failed ('+ err.message +')');
-      console.log(err.stack);
+queue.push (function () {
+  app ('GET', '/prices', function (err) {
+    if (err) {
+      console.log ('API access: failed ('+ err.message +')');
+      console.log (err.stack);
       errors++;
-      process.exit(1);
+      process.exit (1);
     } else {
-      console.log('API access: \033[1m\033[32mok\033[0m');
-      doNext();
+      console.log ('API access: \033[1m\033[32mok\033[0m');
+      doNext ();
     }
   });
 });
@@ -107,21 +107,21 @@ queue.push( function() {
 // fake material to use
 var bogus = {
   domain: {
-    name: 'test-'+ Date.now() +'-delete.me'
+    name: 'test-'+ Date.now () +'-delete.me'
   }
 };
 
 
 // ! POST object
-queue.push( function() {
+queue.push (function () {
   var input = {
     domain: {
       name: bogus.domain.name
     }
   };
-  app( 'POST', '/domains', input, function( err, data, meta ) {
+  app ('POST', '/domains', input, function (err, data, meta) {
     bogus.domain = data.domain;
-    doTest( err, 'POST object', [
+    doTest (err, 'POST object', [
       ['code', meta.statusCode === 201],
       ['type', data && data.domain instanceof Object],
       ['item', data.domain.name === bogus.domain.name]
@@ -130,9 +130,9 @@ queue.push( function() {
 });
 
 // ! GET object
-queue.push( function() {
-  app( 'GET', '/domains/'+ bogus.domain.id, function( err, data ) {
-    doTest( err, 'GET object', [
+queue.push (function () {
+  app ('GET', '/domains/'+ bogus.domain.id, function (err, data) {
+    doTest (err, 'GET object', [
       ['type', data && data.domain instanceof Object],
       ['name', data.domain.name === bogus.domain.name]
     ]);
@@ -140,9 +140,9 @@ queue.push( function() {
 });
 
 // ! GET array object
-queue.push( function() {
-  app( 'GET', '/domains', function( err, data ) {
-    doTest( err, 'GET array object', [
+queue.push (function () {
+  app ('GET', '/domains', function (err, data) {
+    doTest (err, 'GET array object', [
       ['data type', data instanceof Array],
       ['data size', data && data.length >= 1],
       ['item type', data && data[0] && data[0].domain instanceof Object],
@@ -152,18 +152,18 @@ queue.push( function() {
 });
 
 // ! DELETE
-queue.push( function() {
-  app( 'DELETE', '/domains/'+ bogus.domain.id, function( err, data ) {
-    doTest( err, 'DELETE', [
+queue.push (function () {
+  app ('DELETE', '/domains/'+ bogus.domain.id, function (err, data) {
+    doTest (err, 'DELETE', [
       ['data', data === true]
     ]);
   });
 });
 
 // ! Error
-queue.push( function() {
-  app( 'GET', '/domains/'+ bogus.domain.id, function( err ) {
-    doTest( null, 'Error', [
+queue.push (function () {
+  app ('GET', '/domains/'+ bogus.domain.id, function (err) {
+    doTest (null, 'Error', [
       ['type', err && err.message === 'API error']
     ]);
   });
