5dab3fc0d2b4156c811f719eba1de88d2587d62e
Franklin van de Meent <fr@nkl.in>
2015-10-26 22:57:43 +0100

    Test: cleaner test results

--- test.js
+++ test.js
@@ -1,6 +1,9 @@
 var pkg = require ('./package.json');
 var util = require ('util');
 var testStart = Date.now ();
+var errors = 0;
+var queue = [];
+var next = 0;
 
 // Setup
 // set env DNSIMPLE_EMAIL and DNSIMPLE_TOKEN (Travis CI)
@@ -24,16 +27,12 @@ if (pass && otp) {
 var app = require ('./') (acc);
 
 // handle exits
-var errors = 0;
 process.on ('exit', function () {
-  var testTime = Date.now () - testStart;
   if (errors === 0) {
-    console.log ('\n\033[1mDONE, no errors.\033[0m');
-    console.log ('Timing: %s ms\n', testTime);
+    console.log ('\n\u001b[1mDONE, no errors.\u001b[0m\n');
     process.exit (0);
   } else {
-    console.log ('\n\033[1mFAIL, '+ errors +' error'+ (errors > 1 ? 's' : '') +' occurred!\033[0m');
-    console.log ('Timing: %s ms\n', testTime);
+    console.log ('\n\u001b[1mFAIL, ' + errors + ' error' + (errors > 1 ? 's' : '') + ' occurred!\u001b[0m\n');
     process.exit (1);
   }
 });
@@ -41,47 +40,47 @@ process.on ('exit', function () {
 // prevent errors from killing the process
 process.on ('uncaughtException', function (err) {
   console.log ();
-  console.error (err.stack);
-  console.trace ();
+  console.log (err);
+  console.log ();
+  console.log (err.stack);
   console.log ();
   errors++;
 });
 
 // Queue to prevent flooding
-var queue = [];
-var next = 0;
-
 function doNext () {
   next++;
-  if (queue[next]) {
-    queue[next] ();
+  if (queue [next]) {
+    queue [next] ();
   }
 }
 
-// doTest (passErr, 'methods', [
+// doTest( passErr, 'methods', [
 //   ['feeds', typeof feeds === 'object']
-// ]);
+// ])
 function doTest (err, label, tests) {
+  var testErrors = [];
+  var i;
+
   if (err instanceof Error) {
-    console.error (label +': \033[1m\033[31mERROR\033[0m\n');
-    console.error (util.inspect (err, false, 10, true));
+    console.log ('\u001b[1m\u001b[31mERROR\u001b[0m - ' + label + '\n');
+    console.dir (err, { depth: null, colors: true });
     console.log ();
-    console.error (err.stack);
+    console.log (err.stack);
     console.log ();
     errors++;
   } else {
-    var testErrors = [];
-    tests.forEach (function (test) {
-      if (test[1] !== true) {
-        testErrors.push (test[0]);
+    for (i = 0; i < tests.length; i++) {
+      if (tests [i] [1] !== true) {
+        testErrors.push (tests [i] [0]);
         errors++;
       }
-    });
+    }
 
     if (testErrors.length === 0) {
-      console.log (label +': \033[1m\033[32mok\033[0m');
+      console.log ('\u001b[1m\u001b[32mgood\u001b[0m - ' + label);
     } else {
-      console.error (label +': \033[1m\033[31mfailed\033[0m ('+ testErrors.join (', ') +')');
+      console.log ('\u001b[1m\u001b[31mFAIL\u001b[0m - ' + label + ' (' + testErrors.join (', ') + ')');
     }
   }
 
@@ -97,7 +96,7 @@ queue.push (function () {
       errors++;
       process.exit (1);
     } else {
-      console.log ('API access: \033[1m\033[32mok\033[0m');
+      console.log ('\u001b[1m\u001b[32mgood\u001b[0m - API access');
       doNext ();
     }
   });
